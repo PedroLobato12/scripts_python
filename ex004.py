99ece2ed4bc233372f41b9edb72f2ce95e88d63f
PedroLobato12 <153690390+PedroLobato12@users.noreply.github.com>
2024-03-01 20:44:20 -0300

    Novas atividades

--- ex004.py
+++ ex004.py
@@ -1,2 +1,11 @@
-n = float(input('Digite um valor:'))
-print(type)
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(n)
+      )
+print('só tem espaços?', n.isspace())
+print('É um número?', n.isnumeric())
+print('É alfabético?', n.isalpha())
+print('É alfanumérico?', n.isalnum())
+print('Esta em maiúsculas?', n.isupper())
+print('Esta em  minusculas?', n.islower())
+print('Esta capitalizada?', n.istitle())
+
